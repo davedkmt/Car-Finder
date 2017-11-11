da0a983880f3ccf340940465143ab6c387273f54
davemtadesse <31293749+davemtadesse@users.noreply.github.com>
2017-11-11 14:05:03 -0600

    heroku push

--- server.js
+++ server.js
@@ -3,48 +3,69 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-
-
 var app = express();
-// app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.urlencoded({extended: true}));
+
 // Initial port
 // -----------------------------------------------------------------------------
 var PORT = process.env.PORT || 3000;
 
-var answer =
-[
-  {
-  "name": "Mickey Mouse",
-  "photo": "https://www.disneyclips.com/imagesnewb/images/mickey_happy.gif",
-  "scores": [
-  "5",
-  "5",
-  "3",
-  "3",
-  "3",
-  "4",
-  "1",
-  "3",
-  "4",
-  "4"
-  ]
+// Sets up the Express app to handle data parsing
+// -----------------------------------------------------------------------------
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+// localhost to the main page
+// -----------------------------------------------------------------------------
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+// localhost to the survey page
+// -----------------------------------------------------------------------------
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+
+// Initial matches
+// -----------------------------------------------------------------------------
+
+var answer = [{
+    "name": "Mickey Mouse",
+    "photo": "https://www.disneyclips.com/imagesnewb/images/mickey_happy.gif",
+    "scores": [
+      "1",
+      "5",
+      "3",
+      "3",
+      "3",
+      "4",
+      "1",
+      "3",
+      "4",
+      "4"
+    ]
   },
   {
-  "name": "Scooby-doo",
-  "photo": "https://vignette.wikia.nocookie.net/cartoons/images/6/67/Scooby-doo.png/revision/latest?cb=20131106060946",
-  "scores": [
-  "5",
-  "2",
-  "3",
-  "2",
-  "5",
-  "3",
-  "4",
-  "4",
-  "2",
-  "2"
-  ]
+    "name": "Scooby-doo",
+    "photo": "https://vignette.wikia.nocookie.net/cartoons/images/6/67/Scooby-doo.png/revision/latest?cb=20131106060946",
+    "scores": [
+      "5",
+      "2",
+      "3",
+      "2",
+      "5",
+      "3",
+      "4",
+      "4",
+      "2",
+      "2"
+    ]
   }
 ];
 
@@ -70,56 +91,34 @@ app.get("/api/answer:answer?", function(req, res) {
 
 
 app.post("/api/answer:answer?", function(req, res) {
-      var close = 0;
-      var right = {
-        name: "",
-        photo: "",
-        difference: 110
-      };
+  var close = 0;
+  var right = {
+    name: "",
+    photo: "",
+    difference: 51
+  };
 
-      for (var i=0; i < answer.length; i++) {
-           for (var j=0; j< answer[i].scores[j]; j++) {
+  for (var i = 0; i < answer.length; i++) {
+    for (var j = 0; j < answer[i].scores[j]; j++) {
 
-             close += Math.abs(parseInt(req.body.scores[j]) - parseInt(answer[i].scores[j]));
+      close += Math.abs(parseInt(req.body.scores[j]) - parseInt(answer[i].scores[j]));
 
-             if (close <= right.close) {
-               right.name = answer[i].name;
-               right.photo = answer[i].photo;
-               right.difference = close;
+      if (close <= right.difference) {
+        right.name = answer[i].name;
+        right.photo = answer[i].photo;
+        right.difference = close;
 
-             }
-           }
       }
 
-      answer.push(req.body);
+    }
+  }
 
-      res.json(right);
-       console.log(right);
+  answer.push(req.body);
+
+  res.json(right);
+
+  // console.log(right);
 });
-
-
-
-// Sets up the Express app to handle data parsing
-// -----------------------------------------------------------------------------
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-// localhost to the main page
-// -----------------------------------------------------------------------------
-
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "home.html"));
-});
-
-// localhost to the survey page
-// -----------------------------------------------------------------------------
-
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "survey.html"));
-});
-
 
 
 // Checking the port with console
